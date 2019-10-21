9027e53da49283c479f1d719602ea08742efe484
Mirek Kukla <mirekkukla@gmail.com>
2019-10-21 10:32:42 -0700

    quotes

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 /* jshint node: true */
 "use strict";
 
-let express = require('express');
-let fetch = require('node-fetch');
-require('log-timestamp');
+let express = require("express");
+let fetch = require("node-fetch");
+let axios = require("axios");
+require("log-timestamp");
 
 const PORT = 8081;
 const GEO_KEY = "at_IUxTwRiCCI0kwGkZSDm3VGaWxF4LY";
@@ -17,15 +18,15 @@ app.use(express.static(__dirname));
 app.use(express.json());
 
 // Trigger the sending of an email via emailjs
-// Post body should be `{'outcome': 'description of spin outcome'}`
-app.post('/send_email', (req, res) => {
+// Post body should be `{"outcome": "description of spin outcome"}`
+app.post("/send_email", (req, res) => {
   logRequest(req);
 
-  // It's easy to forget you need to set Content-Type if
-  // you're using `fetch` on the front-end
-  let contentHeader = req.header('Content-Type');
+  // It"s easy to forget you need to set Content-Type if
+  // you"re using `fetch` on the front-end
+  let contentHeader = req.header("Content-Type");
   if (contentHeader !== "application/json") {
-    let errorMsg = `Content-Type header must be 'application/json' (${contentHeader})`;
+    let errorMsg = `Content-Type header must be "application/json" (${contentHeader})`;
     res.status("400").send(errorMsg);
     throw new Error(errorMsg);
   }
@@ -34,7 +35,7 @@ app.post('/send_email', (req, res) => {
   let bodyStr = JSON.stringify(req.body);
   let outcome = req.body.outcome;
   if (!outcome) {
-    let errorMsg = `Missing or empty missing 'outcome' parameter (${bodyStr})`;
+    let errorMsg = `Missing or empty missing "outcome" parameter (${bodyStr})`;
     res.status("400").send(errorMsg);
     throw new Error(errorMsg);
   }
@@ -80,9 +81,9 @@ function fetchGeo(req) {
 }
 
 function getIP(req) {
-  // Careful: req.ip doesn't work if we're behind an nginx proxy!
+  // Careful: req.ip doesn"t work if we"re behind an nginx proxy!
   // https://stackoverflow.com/questions/8107856/how-to-determine-a-users-ip-address-in-node
-  let raw_ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
+  let raw_ip = req.header("x-forwarded-for") || req.connection.remoteAddress;
   return raw_ip.split(",")[0].trim();
 }
 
@@ -119,40 +120,40 @@ function sendEmail(outcome, geoInfo) {
     };
 
     let emailJSData = {
-      service_id: 'sendgrid',
+      service_id: "sendgrid",
       template_id: EMAIL_JS_TEMPATE,
       user_id: EMAILJS_KEY,
       template_params: templateParams
     };
 
-    console.log(`Sending email with params '${JSON.stringify(templateParams)}' to EmailJS`);
-    return fetch('https://api.emailjs.com/api/v1.0/email/send', {
-      method: 'POST',
+    console.log(`Sending email with params "${JSON.stringify(templateParams)}" to EmailJS`);
+    return fetch("https://api.emailjs.com/api/v1.0/email/send", {
+      method: "POST",
       body: JSON.stringify(emailJSData),
-      headers: {'Content-Type': 'application/json'}
+      headers: {"Content-Type": "application/json"}
     })
       .then(handleFetchErrors)
       .then(response => response.text())
-      .then(text => console.log('EMAIL SUCCESS!', text))
+      .then(text => console.log("EMAIL SUCCESS!", text))
       .then(_ => templateParams)
-      .catch(error => console.error('EMAIL FAILED...', error));
+      .catch(error => console.error("EMAIL FAILED...", error));
 }
 
 
 // UTILS
 
-// Since `fetch()` doesn't "catch" HTTP errors, we need to handle them ourselves
+// Since `fetch()` doesn"t "catch" HTTP errors, we need to handle them ourselves
 // TODO: share code with front-end
 // https://stackoverflow.com/questions/3225251/how-can-i-share-code-between-node-js-and-the-browser
 // https://stackoverflow.com/questions/950087/how-do-i-include-a-javascript-file-in-another-javascript-file
 // https://medium.com/the-node-js-collection/an-update-on-es6-modules-in-node-js-42c958b890c
 function handleFetchErrors(response) {
-  // 'clone' so we can still read the original `response` downstream)
+  // "clone" so we can still read the original `response` downstream)
   return response.clone().text()
     .then(bodyTxt => {
       if (!response.ok) {
         console.error("Fetch response wasn't OK");
-        throw new Error(`status: '${response.status}', body: '${bodyTxt}'`);
+        throw new Error(`status: "${response.status}", body: "${bodyTxt}"`);
       }
       return response;
     });
